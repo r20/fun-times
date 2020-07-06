9dd9e1d3facd8a492389146dd44bf58ae686ea2f
Josh Rhoades <joshr@radware.com>
2020-07-06 14:54:49 -0400

    Prep for initial internal release, wait for calendar ready before showing anything

--- App.js
+++ App.js
@@ -7,7 +7,7 @@ import { AppearanceProvider, useColorScheme } from 'react-native-appearance'
 
 import { EventsAndMilestonesContextProvider } from './app/context/EventsAndMilestonesContext'
 import AppSettingsContext, { AppSettingsContextProvider } from './app/context/AppSettingsContext'
-import { CalendarProvider } from './app/context/CalendarContext'
+import CalendarContext, { CalendarProvider } from './app/context/CalendarContext'
 import { MyThemeProvider } from './app/context/MyThemeContext'
 import AppBottomTabNavigator from './app/navigation/AppBottomTabNavigator'
 import MyText from './app/components/MyText'
@@ -41,17 +41,24 @@ Still need to add error boundaries too.
 const InnerApp = (props) => {
 
   const appSettingsContext = useContext(AppSettingsContext);
+  const calendarContext = useContext(CalendarContext);
+  // Have these loaded and ready first so colors don't switch right after it opens.
 
-  if (appSettingsContext.isInitialSettingsLoaded) {
-    return <MyThemeProvider><CalendarProvider><EventsAndMilestonesContextProvider ><MenuProvider><AppBottomTabNavigator /></MenuProvider></EventsAndMilestonesContextProvider></CalendarProvider></MyThemeProvider>
+  if (appSettingsContext.isInitialSettingsLoaded && calendarContext.isCalendarReady) {
+    return <EventsAndMilestonesContextProvider ><MenuProvider><AppBottomTabNavigator /></MenuProvider></EventsAndMilestonesContextProvider>
   } else {
     return null;
   }
 }
 
 export default (props) => {
-  return (<AppearanceProvider><AppSettingsContextProvider>
+
+  /* AppLoading tells Expo to keep the app loading screen open (splash screen). But only if it's the first and only component in your app. 
+  I thought it'd be nice to have the splash screen until the theme is ready, but I'm using contexts and I use them to get stuff ready.
+  I need the provider components. */
+
+  return (<AppearanceProvider><AppSettingsContextProvider><MyThemeProvider><CalendarProvider>
     <InnerApp />
-  </AppSettingsContextProvider></AppearanceProvider>);
+  </CalendarProvider></MyThemeProvider></AppSettingsContextProvider></AppearanceProvider>);
 
 }
